a0c9e42d589235b7c78e8270cec7d45ce8ce33a6
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 03:03:04 +0530

    auto: update HomeScreen.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
@@ -20,7 +22,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -30,6 +35,8 @@ import androidx.compose.ui.unit.dp
 import com.elegantgalaxy.tvlauncher.R
 import com.elegantgalaxy.tvlauncher.model.AppCategory
 import com.elegantgalaxy.tvlauncher.model.AppInfo
+import com.elegantgalaxy.tvlauncher.ui.components.AppTile
+import com.elegantgalaxy.tvlauncher.ui.components.SearchBar
 import com.elegantgalaxy.tvlauncher.utils.AppLauncherUtils
 import com.elegantgalaxy.tvlauncher.utils.MockData
 
