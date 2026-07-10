b8e593200d5e793ce0251aabc6f5a5ab3f806cd9
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 04:44:41 +0530

    auto: update HeroBanner.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HeroBanner.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HeroBanner.kt
@@ -39,7 +39,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 private data class HeroBannerItem(
     val title: String,
