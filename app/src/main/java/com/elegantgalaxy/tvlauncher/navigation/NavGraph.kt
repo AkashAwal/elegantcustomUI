1ba20ce5b05bfd7e2531af2d8ede4288b90e4c45
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 04:01:16 +0530

    auto: update NavGraph.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/navigation/NavGraph.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/navigation/NavGraph.kt
@@ -2,7 +2,6 @@ package com.elegantgalaxy.tvlauncher.navigation
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
