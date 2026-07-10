e873f30732f32e632cee0dcecbc8822ce20a1f1a
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 04:56:58 +0530

    auto: update SearchBar.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/components/SearchBar.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/components/SearchBar.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.elegantgalaxy.tvlauncher.ui.theme.SurfaceElevated2
 
