bb0061890dcb9ce3865d7200c53bc4cf4000d99d
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 04:24:11 +0530

    auto: update SideNavRail.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/components/SideNavRail.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/components/SideNavRail.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
