d61cfec92d5170570b6e11dd0b8c7f35f09e743c
AkashAwal <akash.awal.07@gmail.com>
2026-07-11 14:58:29 +0530

    auto: update FeaturedContentRow.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/FeaturedContentRow.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/FeaturedContentRow.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.SettingsInputHdmi
@@ -23,9 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.elegantgalaxy.tvlauncher.ui.theme.SurfaceElevated2
