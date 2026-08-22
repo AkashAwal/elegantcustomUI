e1033a622a00efacb2d8de3d5f8dbaadee78bcd8
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 14:29:40 +0530

    auto: update HomeScreen.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -21,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.palette.graphics.Palette
 import com.elegantgalaxy.tvlauncher.model.AppCategory
