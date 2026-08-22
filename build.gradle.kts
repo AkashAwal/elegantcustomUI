5ad0dcbfb7d0dbbe329704d0fc04c229252e4d64
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 12:06:50 +0530

    auto: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 // so every module resolves the same version without repeating numbers.
 plugins {
     id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
