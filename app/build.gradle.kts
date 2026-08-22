19ebd78baac2d06c2822cef82e8aa25861843011
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 12:05:52 +0530

    auto: update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 // Signing config is read from keystore.properties (git-ignored) so the
