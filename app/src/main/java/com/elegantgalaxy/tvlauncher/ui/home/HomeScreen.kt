7a3c3b6526658e7f5ecdc306b961c8e164b775a7
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 14:30:16 +0530

    auto: update HomeScreen.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.palette.graphics.Palette
 import com.elegantgalaxy.tvlauncher.model.AppCategory
