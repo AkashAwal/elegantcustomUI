ff07aec6ddacb117b9b6d7490e074100ef79e64d
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 12:16:11 +0530

    auto: update HomeScreen.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/HomeScreen.kt
@@ -14,9 +14,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
