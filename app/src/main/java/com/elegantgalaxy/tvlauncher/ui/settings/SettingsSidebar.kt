c8f37df9b8fe69afb8fca52c787f7805850ece14
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 13:31:53 +0530

    auto: update SettingsSidebar.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/settings/SettingsSidebar.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/settings/SettingsSidebar.kt
@@ -123,14 +123,14 @@ fun SettingsSidebar(modifier: Modifier = Modifier) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp)
+                .padding(bottom = 10.dp)
                 .height(1.dp)
                 .background(SurfaceElevated3),
         )
 
-        FullWidthRow(label = "Support", icon = Icons.Filled.SupportAgent, onClick = {})
+        FullWidthRow(label = "Support", icon = Icons.Filled.SupportAgent, onClick = {}, verticalPadding = 12.dp)
 
-        Spacer(Modifier.height(12.dp))
+        Spacer(Modifier.height(8.dp))
 
         FullWidthRow(label = "Contact Us", icon = Icons.Filled.ContactPhone, onClick = {})
     }
