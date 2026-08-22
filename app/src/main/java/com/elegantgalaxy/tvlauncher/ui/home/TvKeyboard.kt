75589311dba849f9007a15b6ec2074c07ff806df
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 17:13:32 +0530

    auto: update TvKeyboard.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/TvKeyboard.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/TvKeyboard.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
