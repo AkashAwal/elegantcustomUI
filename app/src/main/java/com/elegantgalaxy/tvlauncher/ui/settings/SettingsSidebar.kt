27bf446e42364a4caed4f8b5a393f127c652c132
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 13:32:15 +0530

    auto: update SettingsSidebar.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/settings/SettingsSidebar.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/settings/SettingsSidebar.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.elegantgalaxy.tvlauncher.ui.theme.DisplayMode
 import com.elegantgalaxy.tvlauncher.ui.theme.SurfaceElevated1
