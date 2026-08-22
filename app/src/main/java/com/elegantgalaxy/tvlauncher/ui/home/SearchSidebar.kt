ee97062860a8cf57430414d2c86c411fb2d5e893
AkashAwal <akash.awal.07@gmail.com>
2026-08-22 14:22:04 +0530

    auto: update SearchSidebar.kt

--- app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/SearchSidebar.kt
+++ app/src/main/java/com/elegantgalaxy/tvlauncher/ui/home/SearchSidebar.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
